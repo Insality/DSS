eab07b028ae252b3a34379b8f80f8a1f18103153
pepsikat <piratka-mc@mail.ru>
2016-03-20 15:02:48 +0500

    add events_data controller and table

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -3,37 +3,46 @@
 var dssApp = angular.module("dssApp", []);
 
 
-dssApp.controller("DssCtrl", function($scope, $http) {
+dssApp.controller("DssCtrl", function ($scope, $http) {
 
-	$scope.cur_app_name = "";
-	$scope.cur_event_name = "";
-	$scope.cur_stat_name = "";
+    $scope.cur_app_name = "";
+    $scope.cur_event_name = "";
+    $scope.cur_stat_name = "";
 
-	$http.get("http://localhost:7550/").success(function(data, status, headers, config){
-		$scope.app_list = data["response"];
-	}).error(function(){
-	});
+    $http.get("http://localhost:7550/").success(function (data, status, headers, config) {
+        $scope.app_list = data["response"];
+    }).error(function () {});
 
-	$scope.loadEvents = function(cur_app){
-		$scope.cur_app = cur_app;
-		$scope.stat_list = "";
-
-
-		$http.get("http://localhost:7550/" + cur_app).success(function(data, status, headers, config){
-			$scope.event_list = data["response"];
-		}).error(function(){
-		});
-	};
-
-	$scope.loadStats = function(cur_event){
-		$scope.cur_event = cur_event;
+    $scope.loadEvents = function (cur_app) {
+        $scope.cur_app = cur_app;
+        $scope.stat_list = "";
 
 
-		$http.get("http://localhost:7550/"+ $scope.cur_app + "/" + cur_event+'/stat').success(function(data, status, headers, config){
-			$scope.stat_list = data["response"];
-		}).error(function(){
-		});
-	};
+        $http.get("http://localhost:7550/" + cur_app).success(function (data, status, headers, config) {
+            $scope.event_list = data["response"];
+        }).error(function () {});
+    };
+
+    $scope.loadEventData = function (cur_event) {
+        $scope.cur_event = cur_event;
+
+
+        $http.get("http://localhost:7550/" + $scope.cur_app + "/" + cur_event).success(function (data, status, headers, config) {
+            $scope.event_data_list = data;
+            if (data[0]) {
+                $scope.event_data_head = Object.keys(data[0]);
+            }
+            console.log(data);
+        }).error(function () {});
+
+    };
+
+    $scope.loadStats = function (cur_event) {
+        $scope.cur_event = cur_event;
+
+
+        $http.get("http://localhost:7550/" + $scope.cur_app + "/" + cur_event + '/stat').success(function (data, status, headers, config) {
+            $scope.stat_list = data["response"];
+        }).error(function () {});
+    };
 });
-
-
